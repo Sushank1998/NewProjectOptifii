c8366dc7b606c9638e01d7124fab22853f3355f9
sushant31 <sushank.ishwe@gmail.com>
2025-01-08 20:46:54 +0530

    upadte

--- src/pages/pageSection2/Page1.jsx
+++ src/pages/pageSection2/Page1.jsx
@@ -8,8 +8,8 @@ function Page1() {
   return (
     <div className="flex flex-col  flex-wrap ">
       
-      {/* <SubPage1/> */}
-      {/* <Subpage2/> */}
+      <SubPage1/>
+      <Subpage2/>
       <Subpage3/>
       
     </div>
